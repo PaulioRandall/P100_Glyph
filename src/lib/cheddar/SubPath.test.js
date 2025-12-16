091a00eaab895b3bd62c912da4789d2c9b62f895
Paulio <pauliorandall@gmail.com>
2025-12-16 23:31:04 +0000

    Start work on sub path tests

---
+++ src/lib/cheddar/SubPath.test.js
@@ -0,0 +1,10 @@
+import SubPath from './SubPath.js'
+
+describe('SubPath.js', () => {
+	test('', () => {
+				const p = new Path()
+		const cmd = new SubPath('Z')
+
+		expect(cmd.x).toEqual(null)
+	})
+})
