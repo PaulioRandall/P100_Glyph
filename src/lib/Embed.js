8e3240c3f1b973447fbeb556947163906166eb6d
Paulio <pauliorandall@gmail.com>
2025-12-31 01:58:25 +0000

    Create embed function to mimic Go embedding

--- src/lib/Embed.js
+++ src/lib/Embed.js
@@ -1,46 +1,140 @@
-export default function (Clazz, classes = [], fields = []) {
-	Clazz.prototype.constructor
-	for (const EmbedClazz of classes) {
+// Minimalist function to mimic Go style struct embedding.
+//
+// Interesting experiment. Very limited and will probably
+// break when using anything except basic class properties.
+// It's basically a 'decorator' generator (see decorator
+// pattern).
+//
+// Embedding is an alternative to inheritance, and allows
+// for multiple embedded classes, unlike 'extends'. The
+// 'super' keyword will not work, however, you can access
+// the embedded class directly, e.g. 'this._myProp'.
+//
+// You can't store or pass the value of 'super', because
+// it's just an accessor to properties, not a property
+// itself. But you can store and pass the embedded props.
+export default function (classes = [], props = {}) {
+	class Embedded {
+		constructor() {
+			for (const EmbedClazz of classes) {
+				this['_' + EmbedClazz.name] = new EmbedClazz()
+			}
+
+			for (const propName in props) {
+				this['_' + propName] = props[propName]
+			}
+		}
 	}
 
 	for (const EmbedClazz of classes) {
-		embedClass(Clazz, EmbedClazz)
+		embedClass(Embedded, EmbedClazz)
 	}
 
-	return Clazz
+	for (const propName in props) {
+		defineAccessorProp(Embedded, propName)
+	}
+
+	return Embedded
 }
 
-function embedClass(Clazz, EmbedClazz) {
-	const embedProto = Object.getPrototypeOf(EmbedClazz)
-	const embedPropNames = Object.getOwnPropertyNames(embedProto)
+function embedClass(Embedded, EmbedClazz) {
+	const propName = '_' + EmbedClazz.name
+	const subPropNames = Object.getOwnPropertyNames(
+		EmbedClazz.prototype //
+	)
 
-	// TODO: add private field that constructs the EmbedClazz
-	//       on Clazz construction.
-	//['_' + EmbedClazz.constructor.name] = new EmbedClazz()
+	for (const subPropName of subPropNames) {
+		const desc = describeClazzProp(
+			EmbedClazz, //
+			propName, //
+			subPropName //
+		)
 
-	for (const propName of propNames) {
-		if (propName.startsWith('_')) {
+		if (ignoreProp(desc)) {
 			continue
 		}
 
-		const desc = Object.getOwnPropertyDescriptor(proto, propName)
-
-		if (typeof desc.get === 'function') {
-			instance[propName] = function () {
-				return instance[propname]
-			}
+		if (desc.hasGetter || desc.hasSetter) {
+			defineClazzAccessorProp(Embedded, desc)
+			continue
 		}
 
-		if (typeof desc.set === 'function') {
-			instance[propName] = function (v) {
-				instance[propname] = v
-			}
+		if (desc.isFunc) {
+			defineFuncProp(Embedded, desc)
 		}
+	}
+}
 
-		if (typeof desc.value === 'function') {
-			instance[propName] = function (...args) {
-				return instance[propname](...args)
-			}
+function defineAccessorProp(Clazz, name) {
+	const fieldName = '_' + name
+
+	Object.defineProperty(Clazz.prototype, name, {
+		configurable: true,
+		enumerable: false,
+		get: function () {
+			return this[fieldName]
+		},
+		set: function (v) {
+			this[fieldName] = v
+		},
+	})
+}
+
+function ignoreProp(desc) {
+	return desc.subName.startsWith('_')
+}
+
+function describeClazzProp(Clazz, name, subName) {
+	const desc = Object.getOwnPropertyDescriptor(
+		Clazz.prototype, //
+		subName //
+	)
+
+	return {
+		name,
+		subName,
+		hasGetter: typeof desc.get === 'function',
+		hasSetter: typeof desc.set === 'function',
+		isFunc: typeof desc.value === 'function',
+	}
+}
+
+function defineClazzAccessorProp(Clazz, desc) {
+	Object.defineProperty(Clazz.prototype, desc.subName, {
+		configurable: true,
+		enumerable: false,
+		get: makeGetter(desc.hasGetter, desc.name, desc.subName),
+		set: makeSetter(desc.hasSetter, desc.name, desc.subName),
+	})
+}
+
+function makeGetter(hasGetter, name, subName) {
+	if (hasGetter) {
+		return function () {
+			return this[name][subName]
 		}
+	}
+}
+
+function makeSetter(hasSetter, name, subName) {
+	if (hasSetter) {
+		return function (v) {
+			this[name][subName] = v
+		}
+	}
+}
+
+function defineFuncProp(Clazz, desc) {
+	Object.defineProperty(Clazz.prototype, desc.subName, {
+		value: makeFunc(desc),
+		writable: true,
+		enumerable: false,
+		configurable: true,
+	})
+}
+
+function makeFunc(desc) {
+	return function (...args) {
+		return this[desc.name][desc.subName](...args)
 	}
 }
--- src/lib/Embed.test.js
+++ src/lib/Embed.test.js
@@ -4,7 +4,7 @@ class A {
 	_a = 'alpha'
 
 	get a() {
-		return a
+		return this._a
 	}
 
 	set a(v) {
@@ -21,15 +21,85 @@ class A {
 }
 
 class B {
-	// TODO
+	_b = 'beta'
+
+	get b() {
+		return this._b
+	}
+
+	set b(v) {
+		this._b = v
+	}
+
+	getB() {
+		return this._b
+	}
+
+	setB(v) {
+		this._b = v
+	}
 }
 
 describe('Embed.js', () => {
-	test('Adds getters', () => {
-		class AddsGetters extends Embed([A]) {}
+	test('Adds getter', () => {
+		class AddsGetters extends Embed([A, B]) {}
 		const instance = new AddsGetters()
 
-		console.log(instance)
 		expect(instance.a).toEqual('alpha')
+		expect(instance.b).toEqual('beta')
+	})
+
+	test('Adds setter', () => {
+		class AddsSetters extends Embed([A, B]) {}
+		const instance = new AddsSetters()
+
+		instance.a = 'changed alpha'
+		instance.b = 'changed beta'
+
+		expect(instance.a).toEqual('changed alpha')
+		expect(instance.b).toEqual('changed beta')
+	})
+
+	test('Adds functions', () => {
+		class AddsFunctions extends Embed([A, B]) {}
+		const instance = new AddsFunctions()
+
+		expect(typeof instance.getA).toEqual('function')
+		expect(typeof instance.setA).toEqual('function')
+
+		expect(typeof instance.getB).toEqual('function')
+		expect(typeof instance.setB).toEqual('function')
+
+		expect(instance.getA()).toEqual('alpha')
+		expect(instance.getB()).toEqual('beta')
+
+		instance.setA('changed alpha')
+		instance.setB('changed beta')
+
+		expect(instance.a).toEqual('changed alpha')
+		expect(instance.b).toEqual('changed beta')
+	})
+
+	test('Adds props', () => {
+		class AddsProps extends Embed([], {
+			c: 'charlie',
+			d: function () {
+				return 'delta'
+			},
+		}) {
+			getC() {
+				return this._c
+			}
+		}
+
+		const instance = new AddsProps()
+
+		expect(instance._c).toEqual('charlie')
+		expect(instance.c).toEqual('charlie')
+		instance.c = 'barly'
+		expect(instance.getC()).toEqual('barly')
+
+		expect(typeof instance._d).toEqual('function')
+		expect(instance._d()).toEqual('delta')
 	})
 })
